0563d1c7f46f39170036cf6e8ffa01ae70259df5
beomseok <kbs9406@naver.com>
2023-10-16 22:00:55 +0900

    기초연습

--- src/App.js
+++ src/App.js
@@ -2,23 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "hamber";
+  const naver = {
+    name : "네이버",
+    url : "https://naver.com"
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+    <h1 style={{
+      color : "pink",
+      backgroundColor : "blue"
+
+    }}>{name}.<p>{2 + 3}</p></h1>
+    <a href={naver.url}>{naver.name}</a>
+   
+   </>
   );
 }
 
